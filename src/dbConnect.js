d06ed666aebaa22056e99ed1ec0af961e7d14988
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-08-24 07:46:21 +0700

    Build pipeline workflow

--- src/dbConnect.js
+++ src/dbConnect.js
@@ -2,22 +2,22 @@ const mongoose = require('mongoose')
 
 require('dotenv').config()
 
-mongoose.connection.once('open', () => {
-    console.log('MongoDB connection ready!')
-})
-
-mongoose.connection.on('error', (err) => {
-    console.error(err)
-})
-
 const MONGO_URL = 'mongodb://nasa:sEMn0XCFectUJej3@ac-rdk9lei-shard-00-00.tk1fjls.mongodb.net:27017,ac-rdk9lei-shard-00-01.tk1fjls.mongodb.net:27017,ac-rdk9lei-shard-00-02.tk1fjls.mongodb.net:27017/nasa?ssl=true&replicaSet=atlas-j9yahd-shard-0&authSource=admin&retryWrites=true&w=majority'
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
+
 async function mongoConnect() {
-    await mongoose.connect(MONGO_URL)
+    await mongoose.connect(MONGO_URL);
 }
 
 async function mongoDisconnect() {
-    await mongoose.disconnect()
+    await mongoose.disconnect();
 }
 
 module.exports = {
